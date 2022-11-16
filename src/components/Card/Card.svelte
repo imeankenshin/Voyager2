de3009dd16c9a102a402ea34a286881608b8efea
LinoRino <omurakenshin@gmail.com>
2022-11-16 20:34:03 +0900

    Changed: place

---
+++ src/components/Card/Card.svelte
@@ -0,0 +1,9 @@
+<script lang="ts">
+	export let title: string = 'undefined';
+	let className: string = '';
+	export { className as class };
+</script>
+
+<div class="flex rounded-3xl border-4 border-solid border-gray-400 bg-white p-3 {className}">
+	<h3>{title}</h3>
+</div>
--- src/components/icon/Card.svelte
+++
@@ -1,7 +0,0 @@
-<script lang="ts">
-	export let title: string = 'undefined';
-</script>
-
-<div class="rounded-3xl border-2 border-solid bg-white p-3">
-	<h3>{title}</h3>
-</div>
