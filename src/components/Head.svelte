395bacd3279adc46a7fc53bed81904c48fa3db88
LinoRino <omurakenshin@gmail.com>
2022-11-18 09:28:12 +0900

    Fixed: Head setting

--- src/components/Head.svelte
+++ src/components/Head.svelte
@@ -1,14 +1,12 @@
-<script lang="ts">
-	export let title: string = '';
-</script>
-
 <svelte:head>
 	<link rel="stylesheet" href="/src/components/app.scss" />
 	<link rel="website icon" href="/lib/img/App/App_og.png" sizes="48*48" type="image/png" />
 	<!-- <link rel="icon" href="/lib/img/2x/App@2x.png" sizes="94*94" type="image/png" />
 	<link rel="icon" href="/lib/img/3x/App@3x.png" sizes="144*144" type="image/png" /> -->
 	<link rel="apple-touch-icon-precomposed" href="../img/3x/App_og@3x.png" type="image/png" />
-	<title
-		>{title == ' ' ? { title } + ' - Advenccre' : 'Advenccre - The Awesome SVG icon Library'}</title
-	>
+	<meta property="og:locale" content="en_US" />
+	<meta
+		property="og:description"
+		content="Advenccre is a free svg icon library. it is very modern, simple, inspired by SF Symbols"
+	/>
 </svelte:head>
---
+++ src/components/PageConfig.svelte
@@ -0,0 +1,13 @@
+<script lang="ts">
+	export let title: string = '';
+</script>
+
+<svelte:head>
+	<meta
+		property="og:title"
+		content={title == ' ' ? { title } + ' - Advenccre' : 'Advenccre - The Awesome SVG icon Library'}
+	/>
+	<title
+		>{title == ' ' ? { title } + ' - Advenccre' : 'Advenccre - The Awesome SVG icon Library'}</title
+	>
+</svelte:head>
