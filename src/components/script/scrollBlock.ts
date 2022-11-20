e06cb5c6f71d4ca8d6e64bce29ea02ff34e78bbd
LinoRino <omurakenshin@gmail.com>
2022-11-20 18:12:54 +0900

    Fixed: body style

--- src/components/script/scrollBlock.ts
+++ src/components/script/scrollBlock.ts
@@ -2,8 +2,14 @@ export function scrollBlock(a: boolean) {
 	if (typeof document !== 'undefined') {
 		if (a) {
 			document.body.style.overflow = 'hidden';
+			if (document.body.scrollHeight >= window.innerHeight) {
+				document.body.style.paddingRight = '0.75rem';
+			}
 		} else {
 			document.body.style.overflow = 'auto';
+			if (document.body.scrollHeight >= document.body.offsetHeight) {
+				document.body.style.paddingRight = '0';
+			}
 		}
 	}
 }
