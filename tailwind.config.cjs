6fe8ddf4cf693423701fe17a324847d74fe9c932
LinoRino <omurakenshin@gmail.com>
2022-12-13 21:31:51 +0900

    Changed: componentize

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -1,8 +1,27 @@
+const plugin = require('tailwindcss/plugin')
+
 /** @type {import('tailwindcss').Config} */
 module.exports = {
 	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
 	theme: {
-		extend: {},
+		extend: {
+			fontFamily: {
+				marker: "PermanentMarker"
+			},
+			keyframes : {
+				fadein: {
+					"from" :{ translate: "0 4rem", opacity: "0" },
+					"to": { transform: "0 0", opacity: "1" }
+				}
+			},
+			animation : {
+				fadein: "fadein 150ms ease"
+			}
+		},
 	},
-	plugins: [],
+	plugins: [plugin(function({ addVariant }) {
+      addVariant('scroll', '&::-webkit-scrollbar')
+			addVariant('scroll-tb','&::-webkit-scrollbar-thumb')
+			addVariant('scroll-tb-active','&::-webkit-scrollbar-thumb:active')
+    })],
 }
