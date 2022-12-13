6fd8900686fe755ba73b67c14b740eafd745ff60
LinoRino <omurakenshin@gmail.com>
2022-12-13 21:30:49 +0900

    Add: astro-plugin-svelte

--- src/components/Card/Card.svelte
+++ src/components/Card/Card.svelte
@@ -1,9 +1,8 @@
 <script lang="ts">
-	export let title: string = 'undefined';
 	let className: string = '';
 	export { className as class };
 </script>
 
-<div class="flex rounded-3xl border-4 border-solid border-gray-400 bg-white p-3 {className}">
-	<h3>{title}</h3>
+<div class="flex rounded-lg border-2 border-solid border-gray-600 shadow-md p-3 {className}">
+	<slot></slot>
 </div>
