f181e2d06050eb13a0ec8c4e097d91b102b3b545
LinoRino <omurakenshin@gmail.com>
2022-12-15 10:01:12 +0900

    Add: animation

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -12,16 +12,28 @@ module.exports = {
 				fadein: {
 					"from" :{ translate: "0 4rem", opacity: "0" },
 					"to": { transform: "0 0", opacity: "1" }
-				}
+				},
+				fadeinLeft: {
+					"from" :{ translate: "4rem 0", opacity: "0" },
+					"to": { transform: "0 0", opacity: "1" }
+				},
+				fadeinRight: {
+					"from" :{ translate: "-4rem 0", opacity: "0" },
+					"to": { transform: "0 0", opacity: "1" }
+				},
 			},
 			animation : {
-				fadein: "fadein 150ms ease"
-			}
+				fadein: "fadein 1.5s ease",
+				fadeinLeft: "fadeinLeft 1.5s ease",
+				fadeinRight: "fadeinRight 1.5s ease",
+			},
+			
 		},
 	},
-	plugins: [plugin(function({ addVariant }) {
+	plugins: [plugin(function({ addVariant, }) {
       addVariant('scroll', '&::-webkit-scrollbar')
 			addVariant('scroll-tb','&::-webkit-scrollbar-thumb')
 			addVariant('scroll-tb-active','&::-webkit-scrollbar-thumb:active')
-    })],
+    }),
+	],
 }
