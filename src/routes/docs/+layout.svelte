99c236bfd1a08602a788b4d32c56a8928cd908a5
LinoRino <omurakenshin@gmail.com>
2023-02-13 12:03:37 +0900

    Add: documentation

---
+++ src/routes/docs/+layout.svelte
@@ -0,0 +1,18 @@
+<div class="flex">
+	<div
+		role="doc-chapter"
+		class="sticky top-0 left-0 flex h-full w-60 flex-col px-8 py-20 max-md:hidden"
+	>
+		<ul>
+			<li><a href="/docs/usage"><strong>How 2 use</strong></a></li>
+			<li><a href="/docs/design"><strong>Design system</strong></a></li>
+			<li><a href="/docs/history"><strong>History</strong></a></li>
+			<li><a href="/docs/motto"><strong>Motto</strong></a></li>
+		</ul>
+	</div>
+	<main
+		class="flex w-full max-w-4xl justify-center px-12 py-20 scroll:w-3 scroll:bg-transparent scroll-tb:bg-gray-400/30 [&>*]:w-full"
+	>
+		<slot />
+	</main>
+</div>
