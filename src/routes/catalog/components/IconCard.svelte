a80845550866ecde9e44d47736bc88185a56a7a9
LinoRino <omurakenshin@gmail.com>
2023-02-27 12:08:56 +0900

    Remove: unnecessary folder

--- src/routes/catalog/components/IconCard.svelte
+++ src/routes/catalog/components/IconCard.svelte
@@ -1,19 +1,19 @@
 <script lang="ts">
-	import Card from '../../../lib/components/Card/Card.svelte';
-	export let icon: string | undefined | null = '';
-	export let title: string = '';
-	import Chip from '$lib/components/Chip/Chip.svelte';
+	export let title: string;
 </script>
 
-<Card
-	class="m-2 w-32 min-w-[10rem] max-w-[14rem] flex-col transition-all duration-300 hover:-translate-y-2 sm:w-full"
->
-	<div class="-mx-3 -mt-3 h-36 rounded-t-lg bg-gray-400">
-		{icon}
+<div class="icon-card">
+	<div
+		class="-mx-3 -mt-3 grid h-36 place-items-center rounded-t-lg bg-gray-300 text-6xl text-black"
+	>
+		<span class="v2-{title}" />
 	</div>
-	<p class="my-2 text-xl font-medium">{title}</p>
-	<div class="hidden pb-3 sm:block">
-		<Chip label="system" />
-		<Chip label="trash" />
-	</div>
-</Card>
+	<p class="my-2 text-xl font-medium text-white">{title}</p>
+</div>
+
+<style lang="postcss">
+	.icon-card {
+		@apply m-4 flex w-10 flex-col rounded-lg bg-red-500 p-3 transition-all duration-300 hover:-translate-y-2 sm:w-full;
+		box-shadow: 0 6px 0 theme(colors.red.700);
+	}
+</style>
